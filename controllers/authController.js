3c9c3f08d2c7f27241addc79b0eec6269c26b871
apekshitgangarde02 <110719847+apekshitgangarde02@users.noreply.github.com>
2025-05-21 18:38:00 +0530

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,77 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+exports.signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({
+        message: 'User already exists with this email'
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password
+    });
+
+    const token = user.generateAuthToken();
+
+    res.status(201).json({
+      status: 'success',
+      token,
+      data: {
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email
+        }
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: 'Please provide email and password'
+      });
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({
+        message: 'Incorrect email or password'
+      });
+    }
+
+    const token = user.generateAuthToken();
+
+    res.status(200).json({
+      status: 'success',
+      token,
+      data: {
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email
+        }
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+};
